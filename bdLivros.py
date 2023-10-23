0fbdb3d8ad6f940821ebdde3ea755ca7634f73a1
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-10-23 11:04:44 -0400

    Create bdLivros.py

---
+++ bdLivros.py
@@ -0,0 +1,37 @@
+titulos = {
+    "A culpa é das estrelas": {
+        "autor": "John Green",
+        "ano": 2012,
+        "Gênero": "Romance",
+        "Preço": 25.00,
+        "N. Páginas": 313
+    },
+    "Harry Potter": {
+        "autor": "J.K. Rowling",
+        "ano": 1997,
+        "Gênero": "Fantasia",
+        "Preço": 30.00,
+        "N. Páginas": 223
+    },
+    "1984": {
+        "autor": "George Orwell",
+        "ano": 1949,
+        "Gênero": "Utopia",
+        "Preço": 20.00,
+        "N. Páginas": 328
+    },
+    "Dom Casmurro": {
+        "autor": "Machado de Assis",
+        "ano": 1899,
+        "Gênero": "Romance",
+        "Preço": 18.50,
+        "N. Páginas": 256
+    },
+    "O Senhor dos Anéis": {
+        "autor": "J.R.R. Tolkien",
+        "ano": 1954,
+        "Gênero": "Fantasia",
+        "Preço": 35.00,
+        "N. Páginas": 423
+    }   
+}
